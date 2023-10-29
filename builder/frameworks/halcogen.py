67b40f5047f5a4363e3d5d918c573eca64c521fa
Thanh <quang.thanh.ta@pr1meti.de>
2023-10-29 13:23:06 +0100

    feat: Add HALCOGEN example

--- builder/frameworks/halcogen.py
+++ builder/frameworks/halcogen.py
@@ -1,5 +1,8 @@
 from platformio.project.config import ProjectConfig
 
+from SCons.Script import DefaultEnvironment
+
+env = DefaultEnvironment()
 config = ProjectConfig.get_instance()
 
 print(config.get("platformio", "src_dir"))
