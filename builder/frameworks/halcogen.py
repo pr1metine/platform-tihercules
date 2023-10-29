6cd92beb8d6919e7d539eb4d3dfef4d51c21f55b
Thanh <quang.thanh.ta@pr1meti.de>
2023-10-29 13:51:03 +0100

    feat: Use HALCOGEN linker file

--- builder/frameworks/halcogen.py
+++ builder/frameworks/halcogen.py
@@ -1,8 +1,17 @@
 from platformio.project.config import ProjectConfig
-
+from os.path import join, isfile
 from SCons.Script import DefaultEnvironment
+import sys
 
 env = DefaultEnvironment()
 config = ProjectConfig.get_instance()
 
-print(config.get("platformio", "src_dir"))
+src_dir = config.get("platformio", "src_dir")
+link_script = join(src_dir, "HL_sys_link.ld")
+
+if not isfile(link_script):
+    sys.stderr.write(f"No HALCOGEN linker file found {link_script}! Build will most likely fail")
+
+env.Replace(
+    LDSCRIPT_PATH=link_script
+)
